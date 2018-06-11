69f662c6a800399844af3797db7dd04102d94c74
Milica Petković <petkovic.milica262@gmail.com>
2018-06-11 14:09:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// var quotes = [
-// 	"All that we see or seem is but a dream within a dream.-Edgar Allan Poe",
-// 	"Independence is happiness.-Susan B. Anthony",
-// 	"The only true wisdom is in knowing you know nothing.-Socrates",
-// 	"Try to be a rainbow in someone's cloud.-Maya Angelou",
-// 	"It is during our darkest moments that we must focus to see the light.-Aristotle",
-// 	"This too, shall pass.-anonymous",
-// 	"There isn't a way things should be. There's just what happens, and what we do.-Terry Pratchett",
-// 	"Nothing in life is to be feared, it is only to be understood. Now is the time to understand more so that we may fear less.-Marie Curie",
-// 	"There are no facts, only interpretations.-Friedrich Nietzsche"
-// ]
-
-// function newQuote() {
-// 	var randomQuote = Math.floor(Math.random() * (quotes.length));
-// 	document.getElementById("quote").innerHTML = quotes[randomQuote];
-// }
-
-
-// var button = document.getElementById("next");
-
-// button.addEventListener("click", function() {
-// 	var randomQuote = Math.floor(Math.random() * (quotes.length));
-// 	document.getElementById("quote").innerHTML = quotes[randomQuote];
-// });
 
 
 $(document).ready(function() {
@@ -66,4 +42,31 @@ $(document).ready(function() {
 	});
 
 });
+
+
+
+// var quotes = [
+// 	"All that we see or seem is but a dream within a dream.-Edgar Allan Poe",
+// 	"Independence is happiness.-Susan B. Anthony",
+// 	"The only true wisdom is in knowing you know nothing.-Socrates",
+// 	"Try to be a rainbow in someone's cloud.-Maya Angelou",
+// 	"It is during our darkest moments that we must focus to see the light.-Aristotle",
+// 	"This too, shall pass.-anonymous",
+// 	"There isn't a way things should be. There's just what happens, and what we do.-Terry Pratchett",
+// 	"Nothing in life is to be feared, it is only to be understood. Now is the time to understand more so that we may fear less.-Marie Curie",
+// 	"There are no facts, only interpretations.-Friedrich Nietzsche"
+// ]
+
+// function newQuote() {
+// 	var randomQuote = Math.floor(Math.random() * (quotes.length));
+// 	document.getElementById("quote").innerHTML = quotes[randomQuote];
+// }
+
+
+// var button = document.getElementById("next");
+
+// button.addEventListener("click", function() {
+// 	var randomQuote = Math.floor(Math.random() * (quotes.length));
+// 	document.getElementById("quote").innerHTML = quotes[randomQuote];
+// });
 
